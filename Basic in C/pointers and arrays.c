0c6e5e0146cdbf97bad061291c6bdc75440db96d
Tejshree <tj.241094@gmail.com>
2019-04-30 00:02:29 -0700

    Arrays and pointers

---
+++ Basic in C/pointers and arrays.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*int main()
+{
+    int A[] = {2,4,5,8,1};
+    int i;
+    int *p = A;
+    for (int i=0; i<5; i++)
+    {
+        printf("Address of element %d = %d\n",i,&A[i]);
+        printf("Address of element %d = %d\n",i, A+i);
+        printf("Value of element at position %d = %d\n",i,A[i]);
+        printf("Value of element at position %d = %d\n",i,*(A+i));
+    }
+}
+*/
